6633d7116e25677081f4a467e21207892eef2bc6
Vo Ho Viet Khoa <vhvkhoa@Vos-MacBook-Pro.local>
2020-02-17 01:38:50 +0900

    update

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def train_BMN(data_loader, model, optimizer, epoch, bm_mask):
             loss[0].cpu().detach().numpy(),
             loss[2].cpu().detach().numpy(),
             loss[3].cpu().detach().numpy(),
-            loss[1].cpu().detach().numpy())
+            loss[1].cpu().detach().numpy()))
 
         epoch_pemreg_loss += loss[2].cpu().detach().numpy()
         epoch_pemclr_loss += loss[3].cpu().detach().numpy()
