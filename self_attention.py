6df336dbd6108bed833334d075273365516b4640
Vo Ho Viet Khoa <vhvkhoa@Vos-MacBook-Pro.local>
2020-02-17 01:01:09 +0900

    update

--- self_attention.py
+++ self_attention.py
@@ -140,8 +140,8 @@ def modified_multi_head_attention_forward(query,
         if torch.equal(query, key) and torch.equal(key, value):
             # self-attention
             q, k, v = linear(query, in_proj_weight, in_proj_bias).chunk(3, dim=-1)
-            print(in_proj_weight.size(), in_proj_bias.size())
             if torch.sum(torch.isnan(q)).item() > 0:
+                print(torch.mean(query, dim=-1))
                 print('error before in query', torch.sum(torch.isnan(q), dim=-1).squeeze())
             if torch.sum(torch.isnan(k)).item() > 0:
                 print('error before in key', torch.sum(torch.isnan(k), dim=-1).squeeze())
