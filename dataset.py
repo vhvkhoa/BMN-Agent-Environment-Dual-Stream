e347a9f5dcf54e328686df55399b6d32dc07fa13
Vo Ho Viet Khoa <vhvkhoa@Vos-MacBook-Pro.local>
2020-02-18 18:27:52 +0900

    update

--- dataset.py
+++ dataset.py
@@ -29,6 +29,7 @@ def train_collate_fn(batch):
     end_labels = torch.stack(end_labels)
 
     batch_env_lengths = torch.tensor(batch_env_lengths)
+    print(batch_env_lengths)
     batch_agent_box_lengths = torch.stack(batch_agent_box_lengths, dim=0)
     batch_env_features = torch.stack(batch_env_features, dim=0)
 
