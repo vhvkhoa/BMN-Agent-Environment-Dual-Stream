02f1e96c2139aec52bdf8c0804540602018216e6
Vo Ho Viet Khoa <vhvkhoa@gmail.com>
2020-05-04 12:26:32 +0700

    update

--- rescale_videos.py
+++ rescale_videos.py
@@ -1,30 +1,43 @@
 import cv2
 import numpy as np
+import os
+
 
 if __name__ == '__main__':
     target_n_frames = 16 * 100
-    video_path = 'v1-2/train/v_FCFSLuCZKj4.mp4'
-    target_video_path = 'rescaled/v1-2/train/v_FCFSLuCZKj4.mp4'
+    video_dir = 'ActivityNetVideoData/'
+    output_root = 'RescaledVideoData/'
 
-    in_video = cv2.VideoCapture(video_path)
-    num_frames = in_video.get(cv2.CAP_PROP_FRAME_COUNT)
-    width = int(in_video.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height = int(in_video.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    _, frame = in_video.read()
+    for root, _, filenames in os.walk(video_dir):
+        dirname = os.path.join(output_root, os.path.relpath(root, video_dir))
+        if not os.path.isdir(dirname):
+            os.makedirs(dirname)
 
-    out_video = cv2.VideoWriter(
-        filename=target_video_path,
-        fourcc=cv2.VideoWriter_fourcc(*'mp4v'),
-        fps=30.,
-        frameSize=(width, height),
-        isColor=True
-    )
+        for filename in filenames:
+            video_path = os.path.join(root, filename)
+            target_video_path = os.path.join(output_root, os.path.relpath(root, video_dir), filename)
+            video_path = 'v1-2/train/v_FCFSLuCZKj4.mp4'
+            target_video_path = 'rescaled/v1-2/train/v_FCFSLuCZKj4.mp4'
 
-    target_timestamps = np.linspace(0, num_frames - 1, target_n_frames)
-    current_timestamp = 0
-    for timestamp in target_timestamps:
-        timestamp = round(timestamp)
-        while timestamp > current_timestamp:
+            in_video = cv2.VideoCapture(video_path)
+            num_frames = in_video.get(cv2.CAP_PROP_FRAME_COUNT)
+            width = int(in_video.get(cv2.CAP_PROP_FRAME_WIDTH))
+            height = int(in_video.get(cv2.CAP_PROP_FRAME_HEIGHT))
             _, frame = in_video.read()
-            current_timestamp += 1
-        out_video.write(frame)
+
+            out_video = cv2.VideoWriter(
+                filename=target_video_path,
+                fourcc=cv2.VideoWriter_fourcc(*'mp4v'),
+                fps=30.,
+                frameSize=(width, height),
+                isColor=True
+            )
+
+            target_timestamps = np.linspace(0, num_frames - 1, target_n_frames)
+            current_timestamp = 0
+            for timestamp in target_timestamps:
+                timestamp = round(timestamp)
+                while timestamp > current_timestamp:
+                    _, frame = in_video.read()
+                    current_timestamp += 1
+                out_video.write(frame)
