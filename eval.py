41041e8af5bf3ffbd24effbf4f80f1a42807c3d7
Vo Ho Viet Khoa <vhvkhoa@gmail.com>
2020-06-07 11:37:04 +0700

    update

--- eval.py
+++ eval.py
@@ -66,9 +66,9 @@ def evaluate_proposals(cfg):
         subset='validation')
 
     plot_metric(cfg, uniform_average_nr_proposals_valid, uniform_average_recall_valid, uniform_recall_valid)
-    # print("AR@1 is \t", np.mean(uniform_recall_valid[:, 0]))
-    # print("AR@5 is \t", np.mean(uniform_recall_valid[:, 4]))
-    # print("AR@10 is \t", np.mean(uniform_recall_valid[:, 9]))
-    # print("AR@100 is \t", np.mean(uniform_recall_valid[:, -1]))
+    print("AR@1 is \t", np.mean(uniform_recall_valid[:, 0]))
+    print("AR@5 is \t", np.mean(uniform_recall_valid[:, 4]))
+    print("AR@10 is \t", np.mean(uniform_recall_valid[:, 9]))
+    print("AR@100 is \t", np.mean(uniform_recall_valid[:, -1]))
 
     return auc_score
