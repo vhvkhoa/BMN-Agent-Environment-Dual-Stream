0c253eca4ede58cf5e1d123543de99b3f592f11f
Vo Ho Viet Khoa <vhvkhoa@gmail.com>
2020-06-22 02:02:08 +0700

    update

--- config/defaults.py
+++ config/defaults.py
@@ -15,17 +15,17 @@ _C.TRAIN.STEP_PERIOD = 8
 _C.TRAIN.ATTENTION_STEPS = 1
 _C.TRAIN.LR = 0.0001
 _C.TRAIN.CHECKPOINT_FILE_PATH = ''
-_C.TRAIN.VIDEO_ANNOTATION_FILE = '../datasets/thumos14/thumos_annotations.json'
+_C.TRAIN.VIDEO_ANNOTATION_FILE = '../datasets/thumos14/thumos_annotations_test.json'
 _C.TRAIN.LOG_DIR = './agent_env_runs/'
 
 _C.VAL = CfgNode()
-_C.VAL.VIDEO_ANNOTATION_FILE = '../datasets/thumos14/thumos_annotations.json'
+_C.VAL.VIDEO_ANNOTATION_FILE = '../datasets/thumos14/test_annotations.json'
 
 _C.TEST = CfgNode()
 _C.TEST.CHECKPOINT_PATH = 'checkpoints/checkpoint_1/best_auc.pth'
 
 _C.DATA = CfgNode()
-_C.DATA.ANNOTATION_FILE = '../datasets/thumos14/thumos_annotations.json'
+_C.DATA.ANNOTATION_FILE = '../datasets/thumos14/thumos_annotations_test.json'
 _C.DATA.ENV_FEATURE_DIR = '../datasets/thumos14/env_features/'
 _C.DATA.AGENT_FEATURE_DIR = '../datasets/thumos14/agent_features/'
 _C.DATA.RESULT_PATH = './results/results.json'
