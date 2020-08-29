545656235fec7deb74c053466213fb3291a2469a
Your Name <you@example.com>
2020-08-29 12:00:27 -0500

    update

--- config/defaults.py
+++ config/defaults.py
@@ -4,9 +4,9 @@ from fvcore.common.config import CfgNode
 _C = CfgNode()
 
 _C.GPU_IDS = [0]
-_C.MODE = 'training'
+_C.MODE = 'validation'
 _C.USE_ENV = True
-_C.USE_AGENT = True
+_C.USE_AGENT = False
 
 _C.TRAIN = CfgNode()
 _C.TRAIN.NUM_EPOCHS = 10
@@ -15,23 +15,23 @@ _C.TRAIN.STEP_PERIOD = 8
 _C.TRAIN.ATTENTION_STEPS = 1
 _C.TRAIN.LR = 0.0001
 _C.TRAIN.CHECKPOINT_FILE_PATH = ''
-_C.TRAIN.VIDEO_ANNOTATION_FILE = '../datasets/thumos14/thumos_annotations.json'
+_C.TRAIN.VIDEO_ANNOTATION_FILE = '../datasets/activitynet/annotations/train.json'
 _C.TRAIN.LOG_DIR = './agent_env_runs/'
 
 _C.VAL = CfgNode()
-_C.VAL.VIDEO_ANNOTATION_FILE = '../datasets/thumos14/thumos_test_annotations.json'
+_C.VAL.VIDEO_ANNOTATION_FILE = '../datasets/activitynet/annotations/val_1.json'
 
 _C.TEST = CfgNode()
-_C.TEST.CHECKPOINT_PATH = 'checkpoints/checkpoint_9/best_auc.pth'
+_C.TEST.CHECKPOINT_PATH = 'checkpoints/checkpoint_1/best_auc.pth'
 
 _C.DATA = CfgNode()
-_C.DATA.ANNOTATION_FILE = '../datasets/thumos14/thumos_annotations.json'
-_C.DATA.ENV_FEATURE_DIR = '../datasets/thumos14/env_features/'
-_C.DATA.AGENT_FEATURE_DIR = '../datasets/thumos14/agent_features/'
+_C.DATA.ANNOTATION_FILE = '../datasets/activitynet/annotations/activity_net.v1-3.min.json'
+_C.DATA.ENV_FEATURE_DIR = '../datasets/activitynet/new_env_features/'
+_C.DATA.AGENT_FEATURE_DIR = '../datasets/activitynet/new_agent_features/'
 _C.DATA.RESULT_PATH = './results/results.json'
 _C.DATA.FIGURE_PATH = './results/result_figure.jpg'
-_C.DATA.TEMPORAL_DIM = 128
-_C.DATA.FEATURE_DIM = 2048
+_C.DATA.TEMPORAL_DIM = 100
+_C.DATA.FEATURE_DIM = 2304
 _C.DATA.TARGET_FPS = 30
 _C.DATA.SAMPLING_RATE = 16
 
@@ -40,9 +40,9 @@ _C.MODEL.SCORE_PATH = './checkpoints/scores.json'
 _C.MODEL.CHECKPOINT_DIR = 'checkpoints/'
 _C.MODEL.ATTENTION_HEADS = 4
 _C.MODEL.ATTENTION_LAYERS = 1
-_C.MODEL.HIDDEN_DIM_1D = 256  # 256
-_C.MODEL.HIDDEN_DIM_2D = 128  # 128
-_C.MODEL.HIDDEN_DIM_3D = 512  # 512
+_C.MODEL.HIDDEN_DIM_1D = 256 # 256
+_C.MODEL.HIDDEN_DIM_2D = 128 # 128
+_C.MODEL.HIDDEN_DIM_3D = 512 # 512
 
 _C.BMN = CfgNode()
 _C.BMN.NUM_SAMPLES = 32
