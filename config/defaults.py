a84e0abb5514fde9b6f930b2ee02b49d97df7f52
Your Name <you@example.com>
2020-08-30 18:04:15 -0500

    update config

--- config/defaults.py
+++ config/defaults.py
@@ -3,41 +3,41 @@ from fvcore.common.config import CfgNode
 
 _C = CfgNode()
 
-_C.GPU_IDS = [0]
+_C.GPU_IDS = [0,1,2,3]
 _C.MODE = 'validation'
 _C.USE_ENV = True
-_C.USE_AGENT = False
+_C.USE_AGENT = True
 
 _C.TRAIN = CfgNode()
 _C.TRAIN.NUM_EPOCHS = 10
-_C.TRAIN.BATCH_SIZE = 4
-_C.TRAIN.STEP_PERIOD = 8
+_C.TRAIN.BATCH_SIZE = 16
+_C.TRAIN.STEP_PERIOD = 1
 _C.TRAIN.ATTENTION_STEPS = 1
 _C.TRAIN.LR = 0.0001
 _C.TRAIN.CHECKPOINT_FILE_PATH = ''
 _C.TRAIN.VIDEO_ANNOTATION_FILE = '../datasets/activitynet/annotations/train.json'
-_C.TRAIN.LOG_DIR = './agent_env_runs/'
+_C.TRAIN.LOG_DIR = './c3d_runs/'
 
 _C.VAL = CfgNode()
 _C.VAL.VIDEO_ANNOTATION_FILE = '../datasets/activitynet/annotations/val_1.json'
 
 _C.TEST = CfgNode()
-_C.TEST.CHECKPOINT_PATH = 'checkpoints/checkpoint_1/best_auc.pth'
+_C.TEST.CHECKPOINT_PATH = 'c3d_checkpoints/checkpoint_3/best_auc.pth'
 
 _C.DATA = CfgNode()
 _C.DATA.ANNOTATION_FILE = '../datasets/activitynet/annotations/activity_net.v1-3.min.json'
-_C.DATA.ENV_FEATURE_DIR = '../datasets/activitynet/new_env_features/'
-_C.DATA.AGENT_FEATURE_DIR = '../datasets/activitynet/new_agent_features/'
+_C.DATA.ENV_FEATURE_DIR = '../datasets/activitynet/c3d_env_features/'
+_C.DATA.AGENT_FEATURE_DIR = '../datasets/activitynet/c3d_agent_features/'
 _C.DATA.RESULT_PATH = './results/results.json'
 _C.DATA.FIGURE_PATH = './results/result_figure.jpg'
 _C.DATA.TEMPORAL_DIM = 100
-_C.DATA.FEATURE_DIM = 2304
+_C.DATA.FEATURE_DIM = 2048
 _C.DATA.TARGET_FPS = 30
 _C.DATA.SAMPLING_RATE = 16
 
 _C.MODEL = CfgNode()
-_C.MODEL.SCORE_PATH = './checkpoints/scores.json'
-_C.MODEL.CHECKPOINT_DIR = 'checkpoints/'
+_C.MODEL.SCORE_PATH = './c3d_checkpoints/scores.json'
+_C.MODEL.CHECKPOINT_DIR = 'c3d_checkpoints/'
 _C.MODEL.ATTENTION_HEADS = 4
 _C.MODEL.ATTENTION_LAYERS = 1
 _C.MODEL.HIDDEN_DIM_1D = 256 # 256
